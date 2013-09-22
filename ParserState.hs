4e82da5cf980d6bec47f367f6431709f01f9d912
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-09-23 02:10:26 +0800

    去掉用不到的 Var 相關 state 函數

--- ParserState.hs
+++ ParserState.hs
@@ -7,9 +7,6 @@ module ParserState
   , nextLabel
   , newFunc
   , lookupFunc
-  , newVar
-  , lookupVar
-  , localVar
   ) where
 
 import qualified Data.Map as M
@@ -18,20 +15,16 @@ import Lexer (Token)
 
 type FuncTable = M.Map [Char] [Char]
 data ParserState = ParserState
-  { parserVarTable :: M.Map [Char] Int
-  , parserFuncTable :: FuncTable
+  { parserFuncTable :: FuncTable
   , parserLabelCursor :: Int
-  , parserOffsetCursor :: Int
   }
 
 type ParserT m a = StateT ParserState m a
 type Parser a = ParserT IO a
 
 initParserState = ParserState
-  { parserVarTable = M.empty
-  , parserFuncTable = M.fromList [("input", "read"), ("output", "write")]
+  { parserFuncTable = M.fromList [("input", "read"), ("output", "write")]
   , parserLabelCursor = 0
-  , parserOffsetCursor = 0
   }
 
 nextLabel :: Monad m => ParserT m [Char]
@@ -43,16 +36,6 @@ nextLabel = do
     newState = oldState { parserLabelCursor = newLabel }
   put newState
   return $ "label_" ++ show newLabel
-
-nextOffset :: Monad m => ParserT m Int
-nextOffset = do
-  oldState <- get
-  let
-    oldOffset = parserOffsetCursor oldState
-    newOffset = oldOffset + 8
-    newState = oldState { parserOffsetCursor = newOffset }
-  put newState
-  return newOffset
 
 newFunc :: Monad m => [Char] -> ParserT m [Char]
 newFunc name = do
@@ -79,42 +62,4 @@ lookupFunc name = do
   case M.lookup name (parserFuncTable state) of
     Just label -> return label
     Nothing -> fail $ "Unknown function " ++ name
-
-localVar :: Monad m => ParserT m a -> ParserT m a
-localVar m = do
-  oldState <- get
-  put $ oldState {parserVarTable = M.empty}
-
-  res <- m
-
-  newState <- get
-  put $ newState {parserVarTable = parserVarTable oldState, parserOffsetCursor = 0}
-
-  return res
-
-newVar :: Monad m => [Char] -> ParserT m Int
-newVar name = do
-  oldState <- get
-  let
-    oldTable = parserVarTable oldState
-
-  if M.member name oldTable
-    then
-      fail $ "variable " ++ name ++ " redefined"
-    else do
-      newOffset <- nextOffset
-      let
-        newTable = M.insert name newOffset oldTable
-        newState = oldState {parserVarTable = newTable}
-
-      put newState
-      return newOffset
-
-lookupVar :: Monad m => [Char] -> ParserT m Int
-lookupVar name = do
-  state <- get
-  case M.lookup name (parserVarTable state) of
-    Just offset -> return offset
-    Nothing -> fail $ "Unknown variable " ++ name
-
 
--- ParserState2.hs
+++ ParserState2.hs
@@ -7,9 +7,6 @@ module ParserState2
   , nextLabel
   , newFunc
   , lookupFunc
-  , newVar
-  , lookupVar
-  , localVar
   ) where
 
 import qualified Data.Map as M
@@ -18,22 +15,18 @@ import Lexer (Token)
 
 type FuncTable = M.Map [Char] [Char]
 data ParserState = ParserState
-  { parserVarTable :: M.Map [Char] Int
-  , parserFuncTable :: FuncTable
+  { parserFuncTable :: FuncTable
   , parserFinalFuncTable :: FuncTable
   , parserLabelCursor :: Int
-  , parserOffsetCursor :: Int
   }
 
 type ParserT m a = StateT ParserState m a
 type Parser a = ParserT IO a
 
 initParserState finalFuncTable = ParserState
-  { parserVarTable = M.empty
-  , parserFuncTable = M.fromList [("input", "read"), ("output", "write")]
+  { parserFuncTable = M.fromList [("input", "read"), ("output", "write")]
   , parserFinalFuncTable = finalFuncTable
   , parserLabelCursor = 0
-  , parserOffsetCursor = 0
   }
 
 nextLabel :: Monad m => ParserT m [Char]
@@ -45,16 +38,6 @@ nextLabel = do
     newState = oldState { parserLabelCursor = newLabel }
   put newState
   return $ "label_" ++ show newLabel
-
-nextOffset :: Monad m => ParserT m Int
-nextOffset = do
-  oldState <- get
-  let
-    oldOffset = parserOffsetCursor oldState
-    newOffset = oldOffset + 8
-    newState = oldState { parserOffsetCursor = newOffset }
-  put newState
-  return newOffset
 
 newFunc :: Monad m => [Char] -> ParserT m [Char]
 newFunc name = do
@@ -81,42 +64,4 @@ lookupFunc name = do
   return $ case M.lookup name (parserFinalFuncTable state) of
       Just label -> label
       Nothing -> "UnknownFunc" ++ name
-
-localVar :: Monad m => ParserT m a -> ParserT m a
-localVar m = do
-  oldState <- get
-  put $ oldState {parserVarTable = M.empty}
-
-  res <- m
-
-  newState <- get
-  put $ newState {parserVarTable = parserVarTable oldState, parserOffsetCursor = 0}
-
-  return res
-
-newVar :: Monad m => [Char] -> ParserT m Int
-newVar name = do
-  oldState <- get
-  let
-    oldTable = parserVarTable oldState
-
-  if M.member name oldTable
-    then
-      fail $ "variable " ++ name ++ " redefined"
-    else do
-      newOffset <- nextOffset
-      let
-        newTable = M.insert name newOffset oldTable
-        newState = oldState {parserVarTable = newTable}
-
-      put newState
-      return newOffset
-
-lookupVar :: Monad m => [Char] -> ParserT m Int
-lookupVar name = do
-  state <- get
-  case M.lookup name (parserVarTable state) of
-    Just offset -> return offset
-    Nothing -> fail $ "Unknown variable " ++ name
-
 

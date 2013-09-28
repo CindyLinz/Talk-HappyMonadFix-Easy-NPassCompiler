07bb4961171f74d33d62f0cfb1c7bdba1e255bbf
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-09-29 03:24:24 +0800

    better test.hs

--- test.hs
+++ test.hs
@@ -8,57 +8,41 @@ import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
 
-newer :: FilePath -> [FilePath] -> IO Bool
+newer :: (Monad m, MonadIO m) => FilePath -> [FilePath] -> m ()
 newer a bs = do
-  aExist <- fileExist a
-  if aExist
-    then do
-      aStatus <- getFileStatus a
-      let
-        aMTime = modificationTime aStatus
-      foldM
-        (\res b ->
-          if res
-            then do
-              bExist <- fileExist b
-              if bExist
-                then do
-                  bStatus <- getFileStatus b
-                  return $ modificationTime bStatus <= aMTime
-                else
-                  return True
-            else
-              return res
-        )
-        True
-        bs
-    else
-      return False
+  aExist <- liftIO $ fileExist a
+  unless aExist (fail "")
 
-newerThen :: String -> FilePath -> [FilePath] -> IO (Maybe ())
-newerThen command target deps = do
-  ignore <- newer target deps
-  if ignore
-    then do
-      putStrLn $ "skip: " ++ command
-      return $ Just ()
-    else
+  aMTime <- (liftIO $ getFileStatus a) >>= return . modificationTime
+  forM_ bs
+    (\b -> do
+      bExist <- liftIO $ fileExist b
+      when bExist $ do
+        bMTime <- (liftIO $ getFileStatus b) >>= return . modificationTime
+        when (aMTime < bMTime) (fail "")
+    )
+
+needThen :: (MonadIO m, Monad m) => String -> FilePath -> [FilePath] -> m ()
+needThen command target deps = do
+  maybeIgnore <- runMaybeT (newer target deps)
+  if maybeIgnore == Nothing
+    then
       alwaysThen command
+    else do
+      liftIO $ putStrLn $ "skip: " ++ command
 
-alwaysThen :: String -> IO (Maybe ())
+alwaysThen :: (MonadIO m, Monad m) => String -> m ()
 alwaysThen command = do
-  putStrLn $ "execute: " ++ command
-  exitCode <- system command
-  return $ if exitCode == ExitSuccess
-    then Just ()
-    else Nothing
+  liftIO $ putStrLn $ "execute: " ++ command
+  exitCode <- liftIO $ system command
+  unless (exitCode == ExitSuccess) (fail "exec fail")
 
 main = do
   [compilerName, targetName] <- getArgs
 
   maybeResult <- runMaybeT $ do
-    lift $ newerThen ("happy -i -o " ++ compilerName ++ ".hs " ++ compilerName ++ ".y") (compilerName ++ ".hs") [compilerName ++ ".y"]
-    lift $ newerThen
+    needThen ("happy -i -o " ++ compilerName ++ ".hs " ++ compilerName ++ ".y") (compilerName ++ ".hs") [compilerName ++ ".y"]
+    needThen
       ("ghc --make " ++ compilerName)
       compilerName
       [ compilerName ++ ".hs"
@@ -68,15 +52,14 @@ main = do
       , "ParserState2.hs"
       , "ParserState3.hs"
       ]
-    lift $ alwaysThen ("./" ++ compilerName ++ " < " ++ targetName ++ ".code > " ++ targetName ++ ".s")
-    lift $ newerThen ("as -o " ++ targetName ++ ".o " ++ targetName ++ ".s") (targetName ++ ".o") [targetName ++ ".s"]
-    lift $ newerThen ("ld -o " ++ targetName ++ " " ++ targetName ++ ".o") targetName [targetName ++ ".o"]
+    alwaysThen ("./" ++ compilerName ++ " < " ++ targetName ++ ".code > " ++ targetName ++ ".s")
+    needThen ("as -o " ++ targetName ++ ".o " ++ targetName ++ ".s") (targetName ++ ".o") [targetName ++ ".s"]
+    needThen ("ld -o " ++ targetName ++ " " ++ targetName ++ ".o") targetName [targetName ++ ".o"]
 
   if maybeResult == Nothing
     then
       putStrLn "Compile failed."
     else do
       putStrLn "Compile success. Run the test..."
-      _ <- runMaybeT $ lift $ alwaysThen ("./" ++ targetName)
-      return ()
+      void $ runMaybeT $ alwaysThen ("./" ++ targetName)
 
